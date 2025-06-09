7de0925d08a3363a5c76a35597dff509b7d1fe30
ADMIN <ADMIN@PC346>
2025-06-09 10:21:33 +0700

    1

--- StudentManagerWeb/Models/Student.cs
+++ StudentManagerWeb/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,10 @@ namespace StudentManagerWeb.Models
     public class Student
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Chưa nhập họ tên")]
         public string FullName { get; set; }
         public int Age { set; get; }
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
         public string Email { set; get; }
     }
 }
